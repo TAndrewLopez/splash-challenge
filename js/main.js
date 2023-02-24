5dfb96079f243400455d08cc699f9e696ac83a1a
Andrew Dobson <tandrewlopez@gmail.com>
2023-02-23 23:03:33 -0500

    test adding children to pixi scene

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-const app = new PIXI.Application({
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: 0xaaaaaa,
-});
+const root = document.getElementById("root");
 
-document.body.appendChild(app.view);
+const appDraw = () => {
+  root.innerHTML = "";
+
+  const app = new PIXI.Application({
+    width: root.offsetWidth,
+    height: root.offsetHeight,
+    backgroundColor: 0x536878,
+  });
+
+  const text = new PIXI.Text("Under Construction", {
+    fill: 0xffffff,
+  });
+  text.position.x = root.offsetWidth / 2;
+  text.position.y = root.offsetHeight / 2;
+  text.anchor.set(0.5);
+
+  app.stage.addChild(text);
+  root.append(app.view);
+};
+
+window.onload = appDraw;
